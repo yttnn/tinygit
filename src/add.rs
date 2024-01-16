1b7a281b1840ce83135e2f1513cc285fe700debb
yttnn <67893139+yttnn@users.noreply.github.com>
2024-01-16 22:50:17 +0900

    create blob object

--- src/add.rs
+++ src/add.rs
@@ -1,9 +1,11 @@
-use std::{fs::{File, self}, io::Write};
+use std::{fs::{File, self}, io::{Write, BufReader, BufRead}};
 
 use crypto::{sha1::Sha1, digest::Digest};
+use flate2::{Compression, write::ZlibEncoder};
 
 pub fn run_add(file_path: &String) -> Result<(), String> {
   write_index(file_path)?;
+  create_object()?;
   Ok(())
 }
 
@@ -28,6 +30,69 @@ fn write_index(file_path: &String) -> Result<(), String> {
 
   println!("{}", file_text);
   Ok(())
+}
+
+fn create_object() -> Result<(), String> {
+  let index_list = get_index_contents()?;
+  for index in index_list {
+    let path = index.path;
+    let text = match fs::read_to_string(path) {
+      Ok(text) => text,
+      Err(e) => { return Err(e.to_string()); }
+    };
+
+    let blob = format!("blob {}\0{}", text.as_bytes().len(), text);
+    // zlib
+    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+    match e.write_all(&blob.as_bytes()) {
+      Ok(_) => {},
+      Err(e) => { return Err(e.to_string()); }
+    }
+    let compressed = match e.finish() {
+      Ok(compressed) => compressed,
+      Err(e) => { return Err(e.to_string()); }
+    };
+
+    let obj_path = "./.tinygit/objects";
+    let blob_path = format!("{}/{}", obj_path, index.hash);
+    let mut obj_file = File::create(blob_path).unwrap();
+    obj_file.write_all(&compressed).unwrap();
+    
+  }
+  Ok(())
+}
+
+fn get_index_contents() -> Result<Vec<IndexContent>, String> {
+  let index_file = match File::open("./.tinygit/index") {
+    Ok(file) => file,
+    Err(_) => { return Err("index file not exists".to_string()); }
+  };
+
+  let mut contents_list: Vec<IndexContent> = Vec::new();
+  let bufreader = BufReader::new(index_file);
+  for line in bufreader.lines() {
+    let line = line.unwrap();
+    let line_split: Vec<&str> = line.split(" ").collect();
+    let content = IndexContent::new(line_split[0], line_split[1], line_split[2]);
+    contents_list.push(content);
+  }
+  Ok(contents_list)
+}
+
+struct IndexContent {
+  status: String,
+  hash: String,
+  path: String,
+}
+
+impl IndexContent {
+  fn new(status: &str, hash: &str, path: &str) -> Self {
+    Self {
+      status: status.to_string(),
+      hash: hash.to_string(),
+      path: path.to_string(),
+    }
+  }
 }
 
 // #[test]
